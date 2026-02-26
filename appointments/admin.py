97b16f027508ff99cac5f8cafc4b651178169a9e
PrevailUzodinma <prevailuzodinma@gmail.com>
2026-02-26 00:59:25 +0000

    registered appointment models in admin

--- appointments/admin.py
+++ appointments/admin.py
@@ -1,3 +1,8 @@
+# appointments/admin.py
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ["patient", "appointment_date", "appointment_time", "status", "reason"]
+    list_filter = ["appointment_date", "status"]
